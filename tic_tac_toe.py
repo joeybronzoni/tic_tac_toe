08ab856b2a9c9f95590c277fffcc746b6c67a93c
josephskrzysowski <josephskrzysowski@gmail.com>
2017-12-13 17:45:16 -0500

    Quick tic_tac_toe game copy from my jupyter-notebook, cleaning it up

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,22 +4,28 @@
 #Author: Joseph Skrzysowski
 #Date 12/13/17
 #Python3.6 env
+#To run you can:
+#python ./tic_tac_toe.py
+#Or run
 #sudo chmod +x tic_tac_toe.py
 #./tic_tac_toe.py
 #----------------------------------------
-
 """
 1) display instructions/rules for playing the game
 2) create main function for holding all the functionality for the game
 3) define the board using numbers like a key pad using range()
 4) define the winning_combos variable. Which combo of 3 choices define a winner
-5) print board layout
+5) print board layout while nobody's won or its not a tie
+6) ask user to choose a number input using one function to handle both players
+7) insert user input on the board(update the board)
+8) test for winning combination
+9) if game is over run main function unless player choses to quit
 """
 
 def display_instructions():
     """Game Instructions and Header."""
     print(
-        """
+    """
     _____      ___       ____    _     ___     ____   ___   ___
    /_   _\ _  /  _\     /_  _\  / \   /  _\   /_  _\ /   \ |  _|
      | |  | | | |__ --   | |   / | \  | |_  --  | |  | | | | -_
@@ -37,13 +43,12 @@ def display_instructions():
     Player 1 is X
     Player 2 is O
     Three in a Row Wins!
-    Have Fun!!!\n
     """
     )
 
 def tic_tac_toe():
     """
-    Main function to play game.
+    Main function to play game. Defines the board and the combination of moves needed to win.
     """
     display_instructions()
     board = [None] + list(range(1, 10))
@@ -69,9 +74,9 @@ def tic_tac_toe():
 
     def choose_number():
         """
-        Function that runs a while loop for handling the user input. 
+        Function that runs a while loop for handling the user input.
         """
-        while input:
+        while input: #I could just test for True here, but I want to be clear for now
             try:
                 i = int(input())
                 if i in board:
@@ -83,12 +88,12 @@ def tic_tac_toe():
 
     def stop_game():
         """
-        Runs draw_board()-prints the board layout like the number pad on your computer. Tests & tracks the placement of players 'X' & 'O'. 
+        Runs draw_board()-prints the board layout like the number pad on your computer. Tests & tracks the placement of players 'X' & 'O'.
          """
         for row1, row2, row3 in winning_combos:
             if board[row1] == board[row2] == board[row3]:
-                print("Player {0} wins!\n".format(board[row1]))
-                print("Congratulations!\n")
+                print("Player {0} is the winner!\n".format(board[row1]))
+                print("Tadow!!!\n")
                 return True
         num = 9 #My linter seems to be very picky about not assigning 9 to a variable
         if num == sum((placement == 'X' or placement == 'O') for placement in board):
@@ -99,11 +104,11 @@ def tic_tac_toe():
         draw_board()
         if stop_game():
             break
-        print("Player {0} pick your move".format(player))
+        print("Player {0} pick your square".format(player))
         board[choose_number()] = player
         print()
-
-while tic_tac_toe:
+    board = [None] + list(range(1, 10))
+while tic_tac_toe: #I could just test for True here, but I want to be clear for now
     tic_tac_toe()
-    if input("Play again (y/n)\n") != "y":
+    if input("Want to play again? (y/n)\n") != "y":
         break
